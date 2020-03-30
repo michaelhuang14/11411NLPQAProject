c691388e40a5142433057fe84b714ebc1cc3e9f0
lvyiwei1 <ylyu1@andrew.cmu.edu>
2020-03-30 12:54:29 -0400

    Got NER to work

--- src/StringProcessor.py
+++ src/StringProcessor.py
@@ -1,5 +1,9 @@
 from nltk.tokenize import word_tokenize
 import nltk
+nltk.download('punkt')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+nltk.download('averaged_perceptron_tagger')
 
 class StringProcessor:
     # takes in a raw string and outputs a list of tokens
@@ -8,7 +12,13 @@ class StringProcessor:
     # takes in a list of tokens and outputs a list of tuples (token, pos)
     def pos_tag(self, listofstrings):
         return nltk.pos_tag(listofstrings)
-    # takes in a aword and outputs what NER BERT classifies it as
-    def BERT_NER(self, word):
-        #TODO install and add BERT pretrained model
-        return "BLOC"
+    # takes in a sentence and outputs NER of the sentence
+    def ner(self, sentence):
+        return nltk.ne_chunk(self.pos_tag(sentence))
+
+#sp = StringProcessor()
+#sentence = "Michael Huang is a student at Carnegie Mellon University."
+#print(sp.ner(sp.tokenize(sentence)))
+
+
+

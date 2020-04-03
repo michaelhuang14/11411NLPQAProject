f8f7c1d93ccf8e947eea65ec83cbcb269f6a6024
Michael Huang <michaelhuang@Michaels-MacBook-Pro-3.local>
2020-04-03 17:54:37 -0500

    moved np-gram code to master

--- src/StringProcessor.py
+++ src/StringProcessor.py
@@ -1,24 +1,29 @@
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
 import nltk
 nltk.download('punkt')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 nltk.download('averaged_perceptron_tagger')
 
-class StringProcessor:
-    # takes in a raw string and outputs a list of tokens
-    def tokenize(self, string):
-        return word_tokenize(string)
-    # takes in a list of tokens and outputs a list of tuples (token, pos)
-    def pos_tag(self, listofstrings):
-        return nltk.pos_tag(listofstrings)
-    # takes in a sentence and outputs NER of the sentence
-    def ner(self, sentence):
-        return nltk.ne_chunk(self.pos_tag(sentence))
+# takes in a raw string and outputs a list of tokens
+def tokenize(string):
+    return word_tokenize(string)
+# takes in a list of tokens and outputs a list of tuples (token, pos)
+def pos_tag(listofstrings):
+    return nltk.pos_tag(listofstrings)
+# takes in a sentence and outputs NER of the sentence
+def NER(sentence):
+    return nltk.ne_chunk(pos_tag(sentence))
+def sentence_tokenize(string):
+    return sent_tokenize(string)
 
 #sp = StringProcessor()
-#sentence = "Michael Huang is a student at Carnegie Mellon University."
-#print(sp.ner(sp.tokenize(sentence)))
+if __name__ == "__main__":
+    sentence = "Michael Huang is a student at Carnegie Mellon University."
+    result = NER(tokenize(sentence))
+    for chunk in result:
+        print(chunk)
 
 
 
